f7056f7051fda95b0cecb7337a16e756eb9b52b6
Le Topo <astrid.jounang.dev@gmail.com>
2025-04-07 04:16:42 -0400

    Add database credentials file example

---
+++ utils/database/db-credentials.example.h
@@ -0,0 +1,14 @@
+//
+// Created by astrid on 2025-04-07.
+//
+
+#ifndef DB_CREDENTIALS_H
+#define DB_CREDENTIALS_H
+
+#define DB_HOST "" // Hostname or IP address of the database server
+#define DB_PORT 0 // Port number of the database server
+#define DB_USER "" // Username for the database connection
+#define DB_PASSWORD "" // Password for the database connection
+#define DB_NAME "" // Name of the database to connect to
+
+#endif //DB_CREDENTIALS_H
